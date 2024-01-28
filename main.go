d4b643457e4a4c6597670aa041633dbe4b2ffa4c
sidhunt5 <siddhantbadola5@gmail.com>
2024-01-28 11:32:30 -0500

    added timmed quiz logic

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "csv file in format of 'q,a'")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
 	file, err := os.Open(*csvFilename)
 	if err != nil {
@@ -21,16 +23,28 @@ func main() {
 		exit("Failed to parse the provided csv file.")
 	}
 	problems := parseLines(lines)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
 	for i, p := range problems {
-		fmt.Printf("Problem #%d : %s\n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+		fmt.Printf("Problem #%d : %s =  ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d", correct, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
 		}
+
 	}
-	fmt.Printf("You scored %d out of %d", correct, len(problems))
+
 }
 
 func parseLines(lines [][]string) []problem {
